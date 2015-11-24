74a118374027df8336bfbbe422651e16054b0917
Dmytro Soloviov <dsoloviov@frov.jcu.cz>
2015-11-24 16:16:56 +0100

    Update: autocrop performed using the regionprops

--- src/autocrop.m
+++ src/autocrop.m
@@ -4,74 +4,24 @@
 % Location argument is specified as _dir('segm_*.jpg')_ in
 % case of folder or _filename_ in case of single file.
 function autocrop(location)
-    try
-        for file = 1:length(location)
-            display(strcat('Processing: ', location(file).name))
-            if not(isempty(location))
-                batch_autocrop(location(file).name)
-            end
+    for file = 1:length(location)
+        display(strcat('Processing: ', location(file).name))
+        if not(isempty(location))   
+            crop(location(file).name)
         end
-    catch
-        display(strcat('Processing: ', location))
-        batch_autocrop(location)
     end
 end
 
-%% Batch autocrop
+%% Crop
 % Performs automated cropping of segmented image
-function batch_autocrop(filename)
-    % Find the xmin and ymin values of cropping rectangle
+function crop(filename)
     i = imread(filename);
     g = rgb2gray(i);
-    isize = size(g);
-
-    % Calculate ymin value for crop rectangle
-    for row = 1:isize(1)
-        %display(g(row, :)); % display row
-        x = g(row, :);
-        if sum(x) ~= 0 && sum(x) > 5000 % TODO: fix it
-            irow = row; % non-zero row index (y for crop rectangle)
-            break
-        end
-    end
-
-    % Calculate xmin value for crop rectangle
-    for column = 1:isize(2)
-        y = g(:, column);
-        if sum(y) ~= 0 && sum(y) > 5000 % TODO: fix it
-            icolumn = column; % non-zero column index (x for crop rectangle)
-            break
-        end
-    end
-    rect = [icolumn, irow, isize(2), isize(1)];
-    i2 = imcrop(i, rect);
-    imwrite(i2, 'temp.jpg');
-
-    % Find the height and width values of cropping rectangle
-    temp = imread('temp.jpg');
-    g = rgb2gray(temp);
-    isize = size(g);
-
-    % Calculate height value for crop rectangle
-    for row = 1:isize(1)
-        x = g(row, :);
-        if sum(x) == 0
-            height = row; % zero row index (y for crop rectangle)
-            break
-        end
-    end
-
-    % Calculate width value for crop rectangle
-    for column = 1:isize(2)
-        y = g(:, column);
-        if sum(y) == 0
-            width = column; % zero column index (x for crop rectangle)
-            break
-        end
-    end
-
-    rect = [icolumn, irow, width, height];
+    object = bwlabel(g > 40);
+    info = regionprops(object);
+    m = max([info(:).Area]); % find max occupied area
+    index = find([info.Area] == m); % find an index of max occupied area
+    rect = info(index).BoundingBox;
     i2 = imcrop(i, rect);
     imwrite(i2, strcat('crop_', filename));
-    delete('temp.jpg');
 end
--- src/segmentation.m
+++ src/segmentation.m
@@ -7,16 +7,11 @@
 % Location argument is specified as _dir('*.jpg')_ in
 % case of folder or _filename_ in case of single file.
 function segmentation(location, threshold)
-    try
-        for file = 1:length(location)
-            display(strcat('Processing: ', location(file).name))
-            if not(isempty(location))
-                simple_segmentation(location(file).name, threshold)
-            end
+    for file = 1:length(location)
+        display(strcat('Processing: ', location(file).name))
+        if not(isempty(location))
+            simple_segmentation(location(file).name, threshold)
         end
-    catch
-        display(strcat('Processing: ', location))
-        simple_segmentation(location, threshold)
     end
 end
 
@@ -24,11 +19,8 @@ end
 % Performs simple image segmentation
 % based on provided threshold
 function simple_segmentation(filename, threshold)
-    %warning('off', 'MATLAB:MKDIR:DirectoryExists');
-    %mkdir('output');
     i = imread(filename);
     bw = rgb2gray(i) < threshold;
     n = i .* repmat(uint8(bw), [1, 1, 3]);
-    %imwrite(n, fullfile('output', strcat('segm_', filename)));
     imwrite(n, strcat('segm_', filename));
 end
